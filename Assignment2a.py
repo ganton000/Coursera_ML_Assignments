590a65bd1399241ce3c899b7a5685d1ef1fe2924
George Anton <SendittoGeorgeAnton@gmail.com>
2021-09-27 21:20:48 -0400

    Completion of Assignment

--- Assignment2a.py
+++ Assignment2a.py
@@ -252,7 +252,7 @@ mush_df = pd.read_csv('/Users/georgeanton/Desktop/Applied_Data_Science_Python_UM
 mush_df2 = pd.get_dummies(mush_df)
 
 X_mush = mush_df2.iloc[:,2:]
-y_mush = mush_df2.iloc[:,1]
+y_mush = mush_df2.iloc[:,1] #2nd column holds classification ('class' column)
 
 # use the variables X_train2, y_train2 for Question 5
 X_train2, X_test2, y_train2, y_test2 = train_test_split(X_mush, y_mush, random_state=0)
@@ -265,17 +265,148 @@ X_train2, X_test2, y_train2, y_test2 = train_test_split(X_mush, y_mush, random_s
 X_subset = X_test2
 y_subset = y_test2
 
-print(mush_df)
+# Question 5
+# Using X_train2 and y_train2 from the preceeding cell,
+# train a DecisionTreeClassifier with default parameters and random_state=0.
+# What are the 5 most important features found by the decision tree?
+#
+# As a reminder, the feature names are available in the X_train2.columns property,
+# and the order of the features in X_train2.columns matches the order of the feature importance
+# values in the classifier's feature_importances_ property.
+#
+# This function should return a list of length 5 containing the feature names
+# in descending order of importance.
+#
+# Note: remember that you also need to set random_state in the DecisionTreeClassifier.
+
+def answer_five():
+    from sklearn.tree import DecisionTreeClassifier
+    from adspy_shared_utilities import plot_class_regions_for_classifier_subplot
+
+    # def plot_feature_importances(clf, feature_names):
+    #     c_features = len(feature_names)
+    #     plt.barh(range(c_features), clf.feature_importances_)
+    #     plt.xlabel("Feature importance")
+    #     plt.ylabel("Feature name")
+    #     plt.yticks(np.arange(c_features), feature_names)
+
+    global X_train2, X_test2, y_train2, y_test2
+
+    clf = DecisionTreeClassifier(random_state=0).fit(X_train2, y_train2)
+
+    features_list = []
+
+    for i, importance_val in enumerate(clf.feature_importances_):
+        features_list.append([importance_val, X_train2.columns[i]])
+
+
+    top_five_features = sorted(features_list, reverse=1)[:5]
+    top_five_names = [ top_five_features[i][1] for i in range(5)]
+
+
+    # plot_feature_importances(clf, X_train2.columns)
+    # plt.show()
+
+    return top_five_names
+
+
+# lst = answer_five()
+# print(lst)
+
+# Question 6
+# For this question, we're going to use the validation_curve function in sklearn.model_selection
+# to determine training and test scores for a Support Vector Classifier (SVC)
+# with varying parameter values. Recall that the validation_curve function,
+# in addition to taking an initialized unfitted classifier object,
+# takes a dataset as input and does its own internal train-test splits to compute results.
+#
+# Because creating a validation curve requires fitting multiple models,
+# for performance reasons this question will use just a subset of the original mushroom dataset:
+# please use the variables X_subset and y_subset as input to the validation curve function
+# (instead of X_mush and y_mush) to reduce computation time.
+#
+# The initialized unfitted classifier object we'll be using is
+# a Support Vector Classifier with radial basis kernel.
+# So your first step is to create an SVC object with default parameters
+# (i.e. kernel='rbf', C=1) and random_state=0.
+# Recall that the kernel width of the RBF kernel is controlled using the gamma parameter.
+#
+# With this classifier, and the dataset in X_subset, y_subset,
+# explore the effect of gamma on classifier accuracy by using the validation_curve function
+# to find the training and test scores for 6 values of gamma from 0.0001 to 10
+# (i.e. np.logspace(-4,1,6)). Recall that you can specify what scoring metric you want
+# validation_curve to use by setting the "scoring" parameter.
+# In this case, we want to use "accuracy" as the scoring metric.
+#
+# For each level of gamma, validation_curve will fit 3 models on different subsets of the data,
+# returning two 6x3 (6 levels of gamma x 3 fits per level) arrays of the scores for the training
+# and test sets.
+#
+# Find the mean score across the three models for each level of gamma for both arrays,
+# creating two arrays of length 6, and return a tuple with the two arrays.
+
+def answer_six():
+    from sklearn.svm import SVC
+    from sklearn.model_selection import validation_curve
+
+    global X_subset, y_subset
+
+    param_range = np.logspace(-4,1,6)
+
+    train_scores, test_scores = validation_curve(SVC(kernel='rbf', C=1, random_state=0),
+    X_subset, y_subset, param_name = 'gamma', param_range = param_range, scoring='accuracy')
+
+    train_scores_mean = np.mean(train_scores, axis=1)
+    test_scores_mean = np.mean(test_scores, axis=1)
+
+    return (train_scores_mean, test_scores_mean)
+
+
+# Question 7
+# Based on the scores from question 6,
+# what gamma value corresponds to a model that is underfitting
+# (and has the worst test set accuracy)?
+# What gamma value corresponds to a model that is overfitting
+# (and has the worst test set accuracy)?
+# What choice of gamma would be the best choice for a model
+# with good generalization performance on this dataset
+# (high accuracy on both training and test set)?
+#
+# Hint: Try plotting the scores from question 6 to visualize the relationship between gamma
+# and accuracy. Remember to comment out the import matplotlib line before submission.
+#
+# This function should return one tuple with the degree values in this order:
+# (Underfitting, Overfitting, Good_Generalization) Please note there is only one correct solution.
 
 
 
+def answer_seven(x):
+
+
+    train_scores_mean, test_scores_mean = x
+
+
+    plt.title('Validation curve with SVM')
+    plt.xlabel('$\gamma$ gamma')
+    plt.ylabel('accuracy')
+
+    plt.semilogx(np.logspace(-4,1,6), train_scores_mean, label='Training Scores',color='darkorange', lw=2)
+    plt.semilogx(np.logspace(-4,1,6), test_scores_mean,label='Test Scores', color='navy',lw=2)
+
+    plt.fill_between(np.logspace(-4,1,6), train_scores_mean, test_scores_mean,
+    train_scores_mean - test_scores_mean, alpha=0.2, color='darkorange', lw=2)
+
+    plt.legend(loc='best')
+    plt.show()
+
+
+    #return is based on visual from chart
+
+    return (0.001, 10, 0.1)
 
 
 
-
-
-
-
+answer_seven(answer_six())
 
 
 
